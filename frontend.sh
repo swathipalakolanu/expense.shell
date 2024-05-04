7bed9f2cb9fc315e47faeddd0b09c2615c73298f
Mykillerdev swathi <palakolanudevops@gmail.com>
2024-05-04 12:32:55 -0400

    added expence.conf to the frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -5,5 +5,5 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-#comment
+cp expense.conf /ect/nginx/default.d/expense.conf
 systemctl restart nginx
