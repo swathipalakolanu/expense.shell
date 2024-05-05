0edba11e3e2b969e28e9a1bafd95cb94949bc42d
Mykillerdev swathi <palakolanudevops@gmail.com>
2024-05-05 08:04:06 -0400

    adding printing statement (echo) to the frontend content

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,20 @@
+echo -e "/e[32mInstalling the nginx server/e[0m"
 dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+
+echo -e "/e[34m Copying the expense.conf file/e[0m"
 cp expense.conf /ect/nginx/default.d/expense.conf
+
+echo -e "/e[35m Deleting the previous content/e[0m"
+rm -rf /usr/share/nginx/html/*
+
+echo -e "/e[33m Dowloading the frontend content/e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "/e[34m unzip the undowloaded content/e[0m"
+unzip /tmp/frontend.zip
+
+echo -e "/e[34m Restarting the nginx/e[0m"
 systemctl restart nginx
+systemctl start nginx
+
+
