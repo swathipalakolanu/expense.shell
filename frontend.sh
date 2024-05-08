91647cc9eb3ada3012dbd5a076ac85357748a882
Mykillerdev swathi <palakolanudevops@gmail.com>
2024-05-08 12:09:30 -0400

    trouble shooting with frontend content by using redirectories

--- frontend.sh
+++ frontend.sh
@@ -4,7 +4,7 @@ echo -e "\e[32mInstalling the nginx server\e[0m"
 dnf install nginx -y &>>/tmp/expense.log
 echo exit status - $?
 
-echo -e "\e[32mCopying the expense.conf file\e[0m"
+echo -e "\e[32mCopying the expense file\e[0m"
 cp expense.conf /ect/nginx/default.d/expense.conf &>>/tmp/expense.log
 echo exit status - $?
 
@@ -18,11 +18,11 @@ echo exit status - $?
 cd /usr/share/nginx/html
 
 
-echo -e "\e[32mUnzip the undowloaded content\e[0m" &>>/tmp/expense.log
-unzip /tmp/frontend.zip
+echo -e "\e[32mUnzip the undowloaded content\e[0m"
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 echo exit status -  $?
 
-echo -e "\e[32mRestarting the nginx\e[0m" &>>/tmp/expense.log
+echo -e "\e[32mRestarting the nginx\e[0m"
 systemctl restart nginx
 systemctl start nginx
 echo exit status - $?
