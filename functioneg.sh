2f35903dac8dffcceba6f214c3688d7f4ca3cb5e
Mykillerdev swathi <palakolanudevops@gmail.com>
2024-05-08 12:58:56 -0400

    example  for function file adding varaibles and accesing

--- functioneg.sh
+++ functioneg.sh
@@ -1,4 +1,15 @@
 print_hello(){
+  x=20
+
  echo HelloWorld
+ echo Y value is $y
  }
  print_hello
+ echo x value is $x
+ y=30
+ input_function(){
+   echo First argument $0
+   echo Second Argument $2
+   echo All Arguments $*
+   }
+ input_function()
